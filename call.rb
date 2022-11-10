c4ab25cb9e73cca17f61c47b12bfe62d12aa2b01
Dizziolica <carolmarques_7@hotmail.com>
2022-11-10 11:08:59 +0000

    first commit

---
+++ call.rb
@@ -0,0 +1,27 @@
+
+puts "Hello, World!"
+
+puts("Escolha um numro")
+num1 = gets.chomp().to_i 
+puts("Escolha um operador")
+opera = gets.chomp()
+puts("Escolha mais um numero")
+num2 = gets.chomp().to_i
+
+puts "Seu numero é  "
+  
+if opera == "+"
+  puts(num1 + num2)
+
+elsif opera == "/"
+  puts(num1 / num2)
+
+elsif opera == "-"
+  puts(num1 - num2)
+
+else
+  puts num1 * num2
+
+end
+  
+  
